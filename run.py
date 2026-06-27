c5f361635d34986bff4b00ff48538a1302a33157
babajonovasad <babajonovasadfx@gmail.com>
2026-06-28 01:58:36 +0500

    Auto create tables on start

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import os
 from app import create_app
+from app.models.models import db
+
 app = create_app()
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
